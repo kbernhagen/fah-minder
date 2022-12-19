283502dcff2ed07f1283416af0416b79ed839ebc
Kevin Bernhagen <kbernhagen.github@gmail.com>
2022-12-18 22:54:15 -0800

    Use -modfied instead of -dirty in version string

--- Sources/fah-minder/Utility.swift
+++ Sources/fah-minder/Utility.swift
@@ -10,14 +10,14 @@ import Foundation
 func makeVersionString() -> String {
   // return something like git describe --always --dirty
   var version = VCS_TAG ?? "0.0.0"
-  // tag[-tick-gshorthash][-dirty]
+  // tag[-tick-gshorthash][-modified]
   if let tick = VCS_TICK {
     if tick != 0 || VCS_WC_MODIFIED {
       version += "-\(tick)-g\(VCS_SHORT_HASH)"
     }
   }
   if VCS_WC_MODIFIED {
-    version += "-dirty"
+    version += "-modified"
   }
   return version
 }
